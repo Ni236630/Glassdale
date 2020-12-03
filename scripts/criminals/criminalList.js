0275fe166dc328f50c80f544c0e6418e9dcec76e
Ni236630 <nicholas.douso@gmail.com>
2020-12-03 15:32:24 -0600

    fixed render with help

--- scripts/criminals/criminalList.js
+++ scripts/criminals/criminalList.js
@@ -5,61 +5,48 @@ import { criminal } from "./criminal.js";
 const contentTarget = document.querySelector(".criminalsContainer");
 const eventHub = document.querySelector(".container");
 
-// export const criminalList = () => {
- 
-//   getCriminals().then( () =>
-   
-//     useCriminals().forEach((criminalObject) =>{
-    
-//     criminalObject =
-//     `
-     
-//         <section class="criminal__data">
-//         <div>
-//           <h2>${criminalObject.name}</h2>
-//           <p>Age: ${criminalObject.age}</p>
-//           <p>Crime: ${criminalObject.conviction}</p>
-//           <p>Term start:${new Date(criminalObject.incarceration.start).toLocaleDateString('en-US')}</p>
-//           <p>Term start: ${new Date(criminalObject.incarceration.end).toLocaleDateString('en-US')} </p>
-//           </div>
-//         </section>
-      
-//       ` 
-//       contentTarget.innerHTML += criminalObject;
-//     })
-//   )
-  
-    
-// }
+
+
+
+          
 
 //Listen for the custom event from convictionSelect
-eventHub.addEventListener("crimeChosen", event =>{
-  const crimeChosen = event.detail.crimeThatWasChosen
+eventHub.addEventListener("crimeChosen", customEvent =>{
+  const crimeChosen = customEvent.detail.crimeThatWasChosen
+ 
   
   
   if ( crimeChosen !== "0"){
-   const matchingCriminals = useCriminals().filter(criminalFromList =>{ criminalFromList.contains(criminal.conviction === event.detail.crimeThatWasChosen)});
-  return matchingCriminals
-  
-  } render (matchingCriminals)
-  
+   
+   const matchingCriminals = appStateCriminals.filter(criminalFromList => { 
+    
+    return  criminalFromList.conviction === crimeChosen
+     
+    })
+   
+    render(matchingCriminals)
+
+  } 
+   
 })
 
-
-const render = (appStateCriminals) => {
-  const convertedCollection = appStateCriminals.filter(criminalObject =>{ 
-    const criminals = criminal(criminalObject)
-    contentTarget.innerHTML += criminals
-   })
-  
-}
-
+let appStateCriminals = []
 // render ALL criminals initially
 export const criminalList = () => {
   getCriminals()
     .then(() =>{
-      const appStateCriminals = useCriminals()
-        render(appStateCriminals)
+       appStateCriminals = useCriminals()
+      render(appStateCriminals)
       
     })
+  }
+  
+const render = criminalCollection => {
+  
+    
+    contentTarget.innerHTML = `${criminalCollection.map(criminalPerson =>
+      criminal(criminalPerson)
+    ).join("")}
+  `
+  
 }
