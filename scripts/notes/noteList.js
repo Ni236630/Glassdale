422b60b7549d71915cf52e1711e84e6cf58b8828
Ni236630 <nicholas.douso@gmail.com>
2021-01-06 12:08:01 -0600

    refactored code to use dropdown for criminals in form

--- scripts/notes/noteList.js
+++ scripts/notes/noteList.js
@@ -1,29 +1,36 @@
 import { getNotes, useNotes } from "./noteDataProvider.js";
 import { NoteHTMLConverter } from "./note.js";
+import { useCriminals } from "../criminals/criminalProvider.js";
 
-
-const contentTarget = document.querySelector(".noteList")
-
-const eventHub = document.querySelector(".container")
+const contentTarget = document.querySelector(".noteList");
+const eventHub = document.querySelector(".container");
 
 eventHub.addEventListener("showNotesClicked", () => {
-    NoteList()
-})
-eventHub.addEventListener("noteStateChanged", () =>{
-  NoteList()
-})
+  NoteList();
+});
+eventHub.addEventListener("noteStateChanged", () => {
+  NoteList();
+});
 
-const render = (noteArray) => {
-    const allNotesConvertedToStrings = noteArray.map((note) =>  NoteHTMLConverter(note) ).join("")
-
-    contentTarget.innerHTML = allNotesConvertedToStrings
-}
-
+const render = (noteArray, criminals) => {
+    
+  const NotesAsStrings = noteArray
+    .map((note) => {
+      // Find the related criminal
+      const relatedCriminal = criminals.find((criminal) => {
+        return criminal.id === note.criminalId;
+      });
+      note.criminalName = relatedCriminal.name;
+      return NoteHTMLConverter(note);
+    })
+    .join("");
+  contentTarget.innerHTML = NotesAsStrings;
+};
 
 export const NoteList = () => {
-    getNotes()
-        .then(() => {
-            const allNotes = useNotes()
-            render(allNotes)
-        })
-}
+  let criminals = useCriminals();
+  getNotes().then(() => {
+    const allNotes = useNotes();
+    render(allNotes, criminals);
+  });
+};
